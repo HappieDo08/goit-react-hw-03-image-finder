111e8628746174e42449cb6ac8250b87f659b38f
HappieDo08 <aquinogerardoXIII@gmail.com>
2024-07-01 00:36:36 +0800

    initial commit

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
-import BeatLoader from "react-spinners/BeatLoader";
+import React, { Component } from 'react';
+import { TailSpin } from 'react-loader-spinner';
+import styles from './Loader.module.css';
 
-const Loader = () => <div><BeatLoader color="#3f51b5" size="25px" /></div>;
+class Loader extends Component {
+  render() {
+    return (
+      <div className={styles.Loader}>
+        {/* <div className={styles.loaderBox}> */}
+          <TailSpin color="#00BFFF" height={80} width={80} />
+        {/* </div> */}
+      </div>
+    );
+  }
+}
 
 export default Loader;
